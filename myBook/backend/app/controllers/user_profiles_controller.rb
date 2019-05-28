757074f9875187a46fbaf0a9e560fd8f60d37ce1
Grubbs <jgrubbs4@dxc.com>
2019-05-28 10:11:08 -0500

    setup

---
+++ myBook/backend/app/controllers/user_profiles_controller.rb
@@ -0,0 +1,61 @@
+class UserProfilesController < ApplicationController
+  # before_action :set_user_profile, only: [:show, :update, :destroy]
+  wrap_parameters false
+
+
+  # GET /user_profiles
+  def index
+    @user_profiles = UserProfile.all
+
+    render json: @user_profiles
+  end
+
+  # GET /user_profiles/1
+  def show
+    render json: @user_profile
+  end
+
+  # POST /user_profiles
+  def create
+    @user_profile = UserProfile.new(
+      firstname: params[:firstname],
+      lastname: params[:lastname],
+      age: params[:age]
+    )
+
+    if @user_profile.save
+      render json: @user_profile, status: :created, location: @user_profile
+    else
+      render json: @user_profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /user_profiles/1
+  def update
+    if @user_profile.update(
+      firstname: params[:firstname],
+      lastname: params[:lastname],
+      age: params[:age]
+      )
+      render json: @user_profile
+    else
+      render json: @user_profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /user_profiles/1
+  def destroy
+    @user_profile.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user_profile
+      @user_profile = UserProfile.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_profile_params
+      params.require(:user_profile).permit(:firstname, :lastname, :age)
+    end
+end
